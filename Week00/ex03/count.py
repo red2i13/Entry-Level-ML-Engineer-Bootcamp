cfd2639598aa0ed2bf7235b55e0fd7d41a83fc87
red2i13 <redaben2001@gmail.com>
2025-02-25 17:05:20 +0000

    done the first 3 exercice

---
+++ Week00/ex03/count.py
@@ -0,0 +1,31 @@
+import string
+import sys
+def text_analyzer(text = None):
+    '''This function counts the number of upper characters, lower characters,
+punctuation and spaces in a given text'''
+    if(not text):
+        text = input("What is the text to analyze? ")
+    punctuation = 0
+    spaces = 0
+    upper = 0
+    lowercase = 0
+    tot = len(text)
+    for char in text:
+        if(char in string.punctuation):
+            punctuation += 1
+        elif(char.isupper()):
+            upper += 1
+        elif(char.islower()):
+            lowercase += 1
+        elif(char.isspace()):
+            spaces += 1
+    print("The text contains {0} printable character(s)\
+:\n- {1} upper letter(s)\n- {2} lower letter(s)\n\
+- {3} punctuation mark(s)\n- {4} space(s)"\
+    .format(tot, upper, lowercase, punctuation, spaces))
+
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        print("Error the number of args are not correct")
+        sys.exit()
+    text_analyzer(sys.argv[1])
