3104fe61f06e534bd3074feb1649538097ffd101
red2i13 <redaben2001@gmail.com>
2025-03-02 11:44:37 +0000

    clean some exercice

--- Week00/ex07/scraper.py
+++ Week00/ex07/scraper.py
@@ -3,15 +3,13 @@ import sys
 import csv
 from bs4 import BeautifulSoup
 
-print(len(sys.argv))
+
 if(len(sys.argv) != 2):
 	print("Error Num of args")
 	sys.exit()
 
 try:
 	r = requests.get('https://data.1337ai.org/')
-	print("request return code ", r)
-	print()
 except:
 	print("Error fetching this page")
 
